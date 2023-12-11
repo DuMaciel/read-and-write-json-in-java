bc8010d395f1bf41c18cda80a313348174efcb7b
Altran <altranfellipe@gmail.com>
2023-12-11 19:13:10 -0300

    add compete methods to classes

--- CompetitionSimulation/src/Weightlifter.java
+++ CompetitionSimulation/src/Weightlifter.java
@@ -2,6 +2,11 @@ import java.math.BigDecimal;
 
 public class Weightlifter extends Athlete{
 	public BigDecimal compete() {
-		return new BigDecimal("");
+		BigDecimal performance = new BigDecimal("");
+		performance = new BigDecimal(1).add(this.forca.multiply(new BigDecimal(1)))
+				.add(this.resistencia.multiply(new BigDecimal(0.6))).add(this.folego.multiply(new BigDecimal(0.4)))
+				.add(this.flexibilidade.multiply(new BigDecimal(0.3))).subtract(this.peso.divide(new BigDecimal(100)))
+				.subtract(new BigDecimal(this.numeroLesoes).divide(new BigDecimal(10)));
+		return performance;
 	}
 }
